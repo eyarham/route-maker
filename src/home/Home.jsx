fd73ea8c6aadcef98bc84e766cd2528dac341987
Eric Yarham <eric.yarham@gmail.com>
2023-04-16 15:37:26 -0400

    adding coordinate lookup and download results

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,18 +2,19 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Container, CssBaseline } from '@mui/material';
+import { Button, Container, CssBaseline } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { parse } from 'papaparse';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CSVLink } from "react-csv";
 import StopsMap from '../map/StopsMap';
 import Header from './Header';
 import Instructions from './Instructions';
-import { getSampleDocument, getTemplate } from './testData';
+import { getDataWithCoords, getSampleDocument, getSampleDocumentNoCoords, getTemplate } from './testData';
 
 const Home = () => {
   const [parsedData, setParsedData] = useState([]);
+  const [coorArr, setCoorArr] = useState([]);
   const columns = [
     { field: 'name', headerName: 'name', width: 200 },
     { field: 'address', headerName: 'address', width: 200 },
@@ -21,6 +22,20 @@ const Home = () => {
     { field: 'long', headerName: 'long', width: 200 },
     { field: 'lat', headerName: 'lat', width: 200 }
   ]
+
+  useEffect(()=>{
+    if ((parsedData.filter(d => !d.long || !d.lat)).length === 0) {
+      setCoorArr(parsedData.map(d => {
+        if (d.long && d.lat)
+          return [d.long, d.lat];
+        return null;
+      }));
+    }
+    else {
+      setCoorArr([]);
+    }
+  },[parsedData])
+
   const handleChange = e => {
     const file = e.target.files[0]
     parse(file, {
@@ -35,8 +50,18 @@ const Home = () => {
   const getRowId = row => {
     return crypto.randomUUID();
   }
+
+  const onLookupClick = async () => {
+    if (parsedData && parsedData.length > 0 && parsedData.length < 10)//hard limit of ten to prevent big loops
+    {
+      const parsedDataWithCoords = await getDataWithCoords(parsedData);
+        setParsedData(parsedDataWithCoords)
+    }
+  }
+
   const templateData = getTemplate();
   const sampleData = getSampleDocument();
+  const sampleDataNoCoor = getSampleDocumentNoCoords();
   return (
     <Container maxWidth="md">
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
@@ -45,6 +70,7 @@ const Home = () => {
       <Instructions />
       <div><CSVLink data={templateData} filename={"template.csv"}>Download Template</CSVLink></div>
       <div><CSVLink data={sampleData} filename={"sample.csv"}>Download Sample Doc</CSVLink></div>
+      <div><CSVLink data={sampleDataNoCoor} filename={"sampleNoCoor.csv"}>Download Sample Doc No Coords</CSVLink></div>
       <div>
         <input
           className="csv-input"
@@ -55,6 +81,9 @@ const Home = () => {
         />
       </div>
       {parsedData && parsedData.length > 0 &&
+        <div><CSVLink data={parsedData} filename={"routes.csv"}>Download Results</CSVLink></div>
+      }
+      {parsedData && parsedData.length > 0 &&
         <div style={{ height: 400, width: '100%' }}>
           <DataGrid
             getRowId={getRowId}
@@ -64,11 +93,12 @@ const Home = () => {
             rowsPerPageOptions={[7]}
             checkboxSelection
           />
-        </div>}
-      {parsedData && parsedData.length > 0 &&
-
+        </div>
+      }
+      {parsedData && parsedData.length > 0 && coorArr &&
         <div>
-          <StopsMap pinCoords={parsedData.map(d => [d.long, d.lat])} />
+          {coorArr.length === 0 && <Button onClick={onLookupClick}>lookup coordinates</Button>}
+          {coorArr && coorArr.length > 0 && <StopsMap pinCoords={coorArr} />}
         </div>
       }
     </Container>
